1aa69d4ec7e7facc130cb60ce1d33f6ca6003fa4
Chance Farrell <chance.farrell@protonmail.com>
2022-03-13 19:03:21 -0500

    clarified node 16 for GAE

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = ({ env }) => ({});
+module.exports = ({ env }) => ({    
+    upload: {
+        config: {
+          provider: 'strapi-provider-upload-google-cloud-storage',
+          providerOptions: {
+              bucketName: 'intrinsic_bucket',
+              publicFiles: false,
+              uniform: false,
+              basePath: '',
+          },
+        },
+      },
+  }
